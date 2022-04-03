10fa75a5649e7ba8c7f765d6fe2f3fbadf95aae8
Gabriel José Oliveira <78929152+gjoliveira@users.noreply.github.com>
2022-04-03 15:56:20 -0300

    Add files via upload

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  plugins: [require('prettier-plugin-tailwindcss')]
+};
